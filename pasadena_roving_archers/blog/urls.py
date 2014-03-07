97d82df717cc5fc278029a2dc6320ac3505ea8ca
Prasan Samtani <prasan.samtani@hulu.com>
2014-03-06 19:55:46 -0800

    Updates for registration

--- pasadena_roving_archers/blog/urls.py
+++ pasadena_roving_archers/blog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 
 from blog import views
 
-urlpatterns = patterns('',
-    url(r'^register.html', views.register, name='register'),
+urlpatterns = patterns(
+    '',
+    url(r'^register/thanks', views.thanks, name='thanks'),
+    url(r'^register', views.register, name='register'),
     url(r'^$', views.index, name='index')
 )
--- pasadena_roving_archers/blog/views.py
+++ pasadena_roving_archers/blog/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import BlogPost, HeaderElement
+from django.http import HttpResponseRedirect
 
 from django import forms
 
 class RegisterForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
+
+def thanks(request):
+    return render(request, 'blog/thanks.html')
     
 def register(request):
-    form = RegisterForm()
-    return render(request, 'blog/register.html', { 'form': form })
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("thanks/")
+    else:
+        form = RegisterForm()
+        return render(request, '/blog/register.html', { 'form': form })
 
 # Create your views here.
 def index(request):
